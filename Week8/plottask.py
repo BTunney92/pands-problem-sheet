289107cc5561182d0cb46edf781e9260537e69c2
unknown <G00270683@gmit.ie>
2021-03-12 21:37:59 +0000

    Week8

---
+++ Week8/plottask.py
@@ -0,0 +1,29 @@
+# Program that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axis
+# Author: Brendan Tunney
+
+# Import numpy & matplotlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Setting X-Axis range
+
+xpoints = np.array(range(0,4))
+
+#Setting Y-Axis range
+
+ypoints = (xpoints)
+ypoints2 = (xpoints*xpoints)
+ypoints3 = (xpoints*xpoints*xpoints)
+
+#Plotting graph along with labels & line colour
+
+plt.plot(xpoints, ypoints, label= "X", color = "Black")
+plt.plot(xpoints, ypoints2, label= "X-Squared", color = "Green")
+plt.plot(xpoints, ypoints3, label= "X-Cubed", color = "Red")
+plt.legend()
+plt.show()
+
+#Saving file
+
+plt.savefig ("Plot Task.png")
