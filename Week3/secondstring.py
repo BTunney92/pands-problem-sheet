4cd276ef076d49b06c21d30fbff19d27b000b8ab
unknown <G00270683@gmit.ie>
2021-04-12 20:12:47 +0100

    Secondstring correction

--- Week3/secondstring.py
+++ Week3/secondstring.py
@@ -1,6 +1,7 @@
 #Program that asks user to input a sentence and outputs everty second letter in reverse order
 #Author: Brendan Tunney
 
-statement = input("Enter your sentence: ")
+statement = input("Enter your sentence: ") #Setting input sentence 
 
-print (statement [2:5])
+print ("Your sentence in reverse order & with every second letter removed is :")
+print (statement [::-2]) # Extended slices. Negative sign for reverse order
